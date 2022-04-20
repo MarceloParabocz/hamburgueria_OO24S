f20edd9d35e59f10f71efb1df3033e01ad9f6223
macerno@hotmail.com <macerno@hotmail.com>
2022-04-19 22:30:34 -0300

    Initial commit - testando som

---
+++ src/main/java/br/edu/utfpr/hamburgueria_oo24s/HamburgueriaOo24SApplication.java
@@ -0,0 +1,13 @@
+package br.edu.utfpr.hamburgueria_oo24s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HamburgueriaOo24SApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HamburgueriaOo24SApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/edu/utfpr/hamburgueria_oo24s/HamburgueriaOo24SApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.utfpr.hamburgueria_oo24s;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HamburgueriaOo24SApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
